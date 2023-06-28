538d4a2c63008778be2d1f09dde8a9ac4a086a86
arimura <arimura@craid-inc.com>
2023-06-28 17:45:35 +0900

    ログインと会員登録機能

--- apps/kita/config/auth.php
+++ apps/kita/config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'members',
         ],
+        'members' => [
+            'driver' => 'session',
+            'provider' => 'members',
+        ],
     ],
 
     /*
--- apps/kita/routes/web.php
+++ apps/kita/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +18,16 @@ Route::get('/', function () {
     return view('example');
 });
 
-Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// 会員登録ルート
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+
+// ログインルート
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+// ログアウトルート
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
