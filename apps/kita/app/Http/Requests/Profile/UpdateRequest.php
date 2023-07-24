c9fe37f8e259dbab305effceac4d1b8dd9fed994
yoshiki warashina <warashina@craid-inc.com>
2023-07-24 16:48:39 +0900

    profile edit with no duplicate email

--- apps/kita/app/Http/Requests/Profile/UpdateRequest.php
+++ apps/kita/app/Http/Requests/Profile/UpdateRequest.php
@@ -25,7 +25,7 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['email:filter','email:dns'],
+            'email' => ['email:filter','email:dns', 'unique:members'],
         ];
     }
 }
