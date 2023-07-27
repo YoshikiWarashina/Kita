8fcaecb3a61cd1219358ba86072d1c6ffa9cf9ae
yoshiki warashina <warashina@craid-inc.com>
2023-07-27 11:55:53 +0900

    pagination update

--- apps/kita/resources/views/articles/articles.blade.php
+++ apps/kita/resources/views/articles/articles.blade.php
@@ -39,23 +39,46 @@
                             </div>
                         @endforeach
                     </div>
-                    <nav aria-label="Page navigation example">
+                    <nav aria-label="...">
                         <ul class="pagination justify-content-center pt-3">
-                            <li class="page-item{{ $articles->currentPage() == 1 ? ' disabled' : '' }}">
-                                <a class="page-link text-success border-success" href="{{ $articles->previousPageUrl() }}" aria-label="Previous">
-                                    Previous
-                                </a>
-                            </li>
-                            @for ($i = 1; $i <= min(3, $articles->lastPage()); $i++)
-                                <li class="page-item{{ $i == $articles->currentPage() ? ' active' : '' }}">
-                                    <a class="page-link text-success border-success{{ $i == $articles->currentPage() ? ' bg-success' : '' }}" href="{{ $articles->url($i) }}">{{ $i }}</a>
+                            @if ($articles->onFirstPage())
+                                <li class="page-item disabled">
+                                    <span class="page-link text-success border-success" aria-label="前">
+                                        Previous
+                                    </span>
+                                </li>
+                            @else
+                                <li class="page-item text-success border-success">
+                                    <a class="page-link text-success border-success" href="{{ $articles->previousPageUrl() }}" aria-label="前">
+                                        Previous
+                                    </a>
+                                </li>
+                            @endif
+
+                            @php
+                                $startPage = max(1, $articles->currentPage() - 1);
+                                $endPage = min($startPage + 2, $articles->lastPage());
+                            @endphp
+
+                            @for ($page = $startPage; $page <= $endPage; $page++)
+                                <li class="page-item{{ $articles->currentPage() == $page ? ' active' : '' }}">
+                                    <a class="page-link border-success text-success{{ $articles->currentPage() == $page ? ' bg-success' : '' }}" href="{{ $articles->url($page) }}">{{ $page }}</a>
                                 </li>
                             @endfor
-                            <li class="page-item{{ $articles->currentPage() == $articles->lastPage() ? ' disabled' : '' }}">
-                                <a class="page-link text-success border-success" href="{{ $articles->nextPageUrl() }}" aria-label="Next">
-                                    Next
-                                </a>
-                            </li>
+
+                            @if ($articles->hasMorePages())
+                                <li class="page-item">
+                                    <a class="page-link text-success border-success" href="{{ $articles->nextPageUrl() }}" aria-label="次">
+                                        Next
+                                    </a>
+                                </li>
+                            @else
+                                <li class="page-item disabled">
+                                    <span class="page-link text-success border-success" aria-label="次">
+                                        Next
+                                    </span>
+                                </li>
+                            @endif
                         </ul>
                     </nav>
                 </div>
