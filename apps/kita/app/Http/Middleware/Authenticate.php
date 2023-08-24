82a08f28c01f156e5a1e0dec4dd97a026ec509c0
yoshiki warashina <warashina@craid-inc.com>
2023-08-24 12:10:49 +0900

    fix redirectTo method on Authenticate

--- apps/kita/app/Http/Middleware/Authenticate.php
+++ apps/kita/app/Http/Middleware/Authenticate.php
@@ -3,23 +3,24 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            if ($request->is('admin/*')) {
-                return route('admin.login');
-            }else{
-                return route('login');
-            }
+        if ($request->expectsJson()) {
+            return null;
+        } else if ($request->is('admin/*')) {
+            return route('admin.login');
+        } else {
+            return route('login');
         }
     }
 }
